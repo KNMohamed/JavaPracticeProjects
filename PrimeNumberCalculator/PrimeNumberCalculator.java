5f70ed61635ffd9ad2a2e74b8950e2b21c1abf74
Khalid Mohamed <kmohamed@uoguelph.ca>
2018-07-15 15:50:25 -0400

    Retrieves Prime Numbers
    N^2 Complexity for printing all prime numbers up to a given upper limit

---
+++ PrimeNumberCalculator/PrimeNumberCalculator.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class PrimeNumberCalculator{
+
+    private int num;
+    
+    public PrimeNumberCalculator(int num){
+        this.num = num;
+    }
+
+  public static void main(String[] args)
+  {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter an upper limit to print prime integers");
+    int x = in.nextInt();
+    PrimeNumberCalculator.PrintPrimeNumbers(x);                    
+  }
+  
+  //Check if an integer is indeed a prime number
+  public static boolean isPrime(int x)
+  {
+    if(x <= 0) return false;
+    if(x == 1) return false;
+    for(int i = 2; i < x; i++){
+      if(x % i == 0){
+      	return false;
+      }
+    }
+    return true;
+  }
+  
+  
+  public static void PrintPrimeNumbers(int upperLimit)
+  {
+    for(int i = 1; i <= upperLimit; i++){
+      if(isPrime(i)){
+        System.out.print(i + ", ");
+      }
+  	}
+  }
+}
